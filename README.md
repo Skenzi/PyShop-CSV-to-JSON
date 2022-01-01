ec91edf5f23db5b4e493e5bdde48966d7dca430b
Skenzi <fitronavm@gmail.com>
2022-01-01 22:03:55 +0500

    update tsconfig; update csvtojson in dir bin and README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # PyShop-CSV-to-JSON
+
+Converter of files from the format .csv to format .json
+
+## Installation
+
+```
+npm run build
+```
+
+## Use
+
+```
+npm run convert [pathToCsv] [pathForResult]
+```
---
+++ bin/csvtojson.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const {writeJson} = require("../built/index");
+
+program
+.arguments('<filepathCsv> <filepathResult>')
+.action((filepathCsv, filepathResult) => {
+    writeJson(filepathCsv, filepathResult)
+    console.log('file created');
+});
+
+program.parse(process.argv);
