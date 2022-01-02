0e2ad8aa8aa116fc5b2d8fd2e0992859ec492298
Skenzi <fitronavm@gmail.com>
2022-01-02 21:11:27 +0500

    update bin

--- bin/csvtojson.js
+++ bin/csvtojson.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const { writeJson } = require('../built/index');
+const { writeJson } = require('../built/index.js');
 
 program
   .arguments('<filepathCsv> <filepathResult>')
