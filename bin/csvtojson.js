494a79e764cc43541f7d4499fa480a8191c5d1f9
Skenzi <fitronavm@gmail.com>
2022-01-02 21:14:34 +0500

    lint

--- bin/csvtojson.js
+++ bin/csvtojson.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const { writeJson } = require('../built/index.js');
+const { writeJson } = require('../built/index');
 
 program
   .arguments('<filepathCsv> <filepathResult>')
